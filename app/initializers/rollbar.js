0d48cbe01e4702e36a730a66783a30665e8b0bb0
Dave Wasmer <davewasmer@gmail.com>
2015-04-13 00:10:14 -0400

    fix null checks

--- app/initializers/rollbar.js
+++ app/initializers/rollbar.js
@@ -6,28 +6,28 @@ export default {
   initialize: function() {
     var errorLogger = Ember.Logger.error;
     Ember.Logger.error = function() {
-      if (Rollbar) {
+      if (window.Rollbar) {
         Rollbar.error.apply(Rollbar, arguments);
       }
       errorLogger.apply(this, arguments);
     };
     var warnLogger = Ember.Logger.warn;
     Ember.Logger.warn = function() {
-      if (Rollbar) {
+      if (window.Rollbar) {
         Rollbar.warning.apply(Rollbar, arguments);
       }
       warnLogger.apply(this, arguments);
     };
     var infoLogger = Ember.Logger.info;
     Ember.Logger.info = function() {
-      if (Rollbar) {
+      if (window.Rollbar) {
         Rollbar.info.apply(Rollbar, arguments);
       }
       infoLogger.apply(this, arguments);
     };
     var debugLogger = Ember.Logger.debug;
     Ember.Logger.debug = function() {
-      if (Rollbar) {
+      if (window.Rollbar) {
         Rollbar.debug.apply(Rollbar, arguments);
       }
       debugLogger.apply(this, arguments);
