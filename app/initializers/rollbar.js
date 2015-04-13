899acd558c8e44dddee85c0979477bcd180c5847
Dave Wasmer <davewasmer@gmail.com>
2015-04-13 00:03:41 -0400

    add checks for Rollbar global

--- app/initializers/rollbar.js
+++ app/initializers/rollbar.js
@@ -6,22 +6,30 @@ export default {
   initialize: function() {
     var errorLogger = Ember.Logger.error;
     Ember.Logger.error = function() {
-      Rollbar.error.apply(Rollbar, arguments);
+      if (Rollbar) {
+        Rollbar.error.apply(Rollbar, arguments);
+      }
       errorLogger.apply(this, arguments);
     };
     var warnLogger = Ember.Logger.warn;
     Ember.Logger.warn = function() {
-      Rollbar.warning.apply(Rollbar, arguments);
+      if (Rollbar) {
+        Rollbar.warning.apply(Rollbar, arguments);
+      }
       warnLogger.apply(this, arguments);
     };
     var infoLogger = Ember.Logger.info;
     Ember.Logger.info = function() {
-      Rollbar.info.apply(Rollbar, arguments);
+      if (Rollbar) {
+        Rollbar.info.apply(Rollbar, arguments);
+      }
       infoLogger.apply(this, arguments);
     };
     var debugLogger = Ember.Logger.debug;
     Ember.Logger.debug = function() {
-      Rollbar.debug.apply(Rollbar, arguments);
+      if (Rollbar) {
+        Rollbar.debug.apply(Rollbar, arguments);
+      }
       debugLogger.apply(this, arguments);
     };
   }
