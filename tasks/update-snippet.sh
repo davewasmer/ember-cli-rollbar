5d5aa45c2e7ba48bbd9a50f4feeb1405b1ebecc1
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2016-04-27 12:08:03 +0300

    Fix `tasks/update-snippet.sh`

--- tasks/update-snippet.sh
+++ tasks/update-snippet.sh
@@ -2,13 +2,13 @@ snippeturl="https://raw.githubusercontent.com/rollbar/rollbar.js/master/dist/rol
 packageurl="https://raw.githubusercontent.com/rollbar/rollbar.js/master/package.json"
 
 echo "/*jshint ignore:start*/" > vendor/rollbar.js
-ocho "window._rollbarConfig = ROLLBAR_CONFIG;" > vendor/rollbar.js
+echo "window._rollbarConfig = ROLLBAR_CONFIG;" > vendor/rollbar.js
 curl --silent $snippeturl >> vendor/rollbar.js
 echo "/*jshint ignore:end*/" >> vendor/rollbar.js
 if $(git diff-index --quiet --cached HEAD); then
   echo "Committing changes ..."
   version=$(curl --silent $packageurl | grep 'version": "' | head -n 1 | cut -d '"' -f 4)
-  git add addon/snippet.js
+  git add vendor/rollbar.js
   git commit -m "update rollbar snippet to version $version"
   echo "Publishing changes ..."
   npm version patch -m "update rollbar snippet to version $version"
