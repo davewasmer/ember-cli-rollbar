dc8fb9580e18633d1c5915cd30974fe8c773725d
Mike North <michael.l.north@gmail.com>
2016-05-30 12:51:36 +0800

    Move rollbar.js to client folder

--- tasks/update-snippet.sh
+++ tasks/update-snippet.sh
@@ -1,10 +1,10 @@
 snippeturl="https://raw.githubusercontent.com/rollbar/rollbar.js/master/dist/rollbar.snippet.js"
 packageurl="https://raw.githubusercontent.com/rollbar/rollbar.js/master/package.json"
 
-echo "/*jshint ignore:start*/" > vendor/rollbar.js
-ocho "window._rollbarConfig = ROLLBAR_CONFIG;" > vendor/rollbar.js
-curl --silent $snippeturl >> vendor/rollbar.js
-echo "/*jshint ignore:end*/" >> vendor/rollbar.js
+echo "/*jshint ignore:start*/" > client/rollbar.js
+echo "window._rollbarConfig = ROLLBAR_CONFIG;" > client/rollbar.js
+curl --silent $snippeturl >> client/rollbar.js
+echo "/*jshint ignore:end*/" >> client/rollbar.js
 if $(git diff-index --quiet --cached HEAD); then
   echo "Committing changes ..."
   version=$(curl --silent $packageurl | grep 'version": "' | head -n 1 | cut -d '"' -f 4)
