0d872deb36b90b9ff96388997c531fe1d4bdf44c
Tony Miller <mcfiredrill@gmail.com>
2017-08-07 13:44:09 +0900

    update to work with fastboot 1.0
    See the upgrade guide, basically the environment variable has been
    removed.
    https://gist.github.com/kratiahuja/d22de0fb1660cf0ef58f07a6bcbf1a1c#using-processenvember_cli_fastboot-to-run-import-in-fastboot-build

--- index.js
+++ index.js
@@ -6,45 +6,51 @@ var merge = require('lodash/merge');
 var replace = require('broccoli-string-replace');
 var mergeTrees = require('broccoli-merge-trees');
 var path = require('path');
+const fastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-cli-rollbar',
-  included: function(app) {
+  options: {
+    nodeAssets: {
+      'rollbar': {
+        enabled: this.enabled,
+        import: {
+          include: ['vendor/rollbar.js', 'vendor/rollbar-module.js'],
+          processTree(input) {
+            return fastbootTransform(input);
+          }
+        }
+      }
+    }
+  },
+  included(app) {
     var config = this.project.config(this.app.env).rollbar || {};
     var defaultEnabled = this.app.env !== 'development' && this.app.env !== 'test';
-    var enabled = config.enabled == null ? defaultEnabled : config.enabled;
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      app.import('vendor/rollbar.js', {
-        prepend: true
-      });
-      app.import('vendor/rollbar-module.js');
-    }
+    this.enabled = config.enabled == null ? defaultEnabled : config.enabled;
+
+    this._super.included.apply(this, arguments);
   },
   treeForVendor: function(vt) {
     var vendorTree = this._super.treeForVendor(vt);
 
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      var config = this.project.config(this.app.env).rollbar || {};
-      config = merge({
-        enabled: this.app.env !== 'development' && this.app.env !== 'test',
-        captureUncaught: true,
-        payload: {
-          environment: this.app.env
-        }
-      }, config);
-      var clientTree = replace(path.join(__dirname, 'client'), {
-        files: ['rollbar.js'],
-        pattern: {
-          match: /ROLLBAR_CONFIG/g,
-          replacement: JSON.stringify(config)
-        }
-      });
+    var config = this.project.config(this.app.env).rollbar || {};
+    config = merge({
+      enabled: this.app.env !== 'development' && this.app.env !== 'test',
+      captureUncaught: true,
+      payload: {
+        environment: this.app.env
+      }
+    }, config);
+    var clientTree = replace(path.join(__dirname, 'client'), {
+      files: ['rollbar.js'],
+      pattern: {
+        match: /ROLLBAR_CONFIG/g,
+        replacement: JSON.stringify(config)
+      }
+    });
 
-      return mergeTrees([vendorTree, clientTree], {
-        overwrite: true
-      });
-    } else {
-      return vendorTree;
-    }
+    return mergeTrees([vendorTree, clientTree], {
+      overwrite: true
+    });
   }
 };
