30c37becf9bc3fad31ad4d71a5844b11764deaab
Jeff Jewiss <jeffjewiss@gmail.com>
2017-10-10 02:09:10 +0100

    wrap initializer in fastboot check

--- app/initializers/rollbar.js
+++ app/initializers/rollbar.js
@@ -4,37 +4,39 @@ import Ember from 'ember';
 export default {
   name: 'rollbar',
   initialize: function() {
-    var errorLogger = Ember.Logger.error;
-    Ember.Logger.error = function() {
-      var args = Array.prototype.slice.call(arguments),
+    if (typeof FastBoot === 'undefined') {
+      var errorLogger = Ember.Logger.error;
+      Ember.Logger.error = function() {
+        var args = Array.prototype.slice.call(arguments),
           err = isError(args[0]) ? args[0] : new Error(stringify(args));
 
-      if (window.Rollbar) {
-        Rollbar.error.call(Rollbar, err);
-      }
-      errorLogger.apply(this, arguments);
-    };
-    var warnLogger = Ember.Logger.warn;
-    Ember.Logger.warn = function() {
-      if (window.Rollbar) {
-        Rollbar.warning.apply(Rollbar, arguments);
-      }
-      warnLogger.apply(this, arguments);
-    };
-    var infoLogger = Ember.Logger.info;
-    Ember.Logger.info = function() {
-      if (window.Rollbar) {
-        Rollbar.info.apply(Rollbar, arguments);
-      }
-      infoLogger.apply(this, arguments);
-    };
-    var debugLogger = Ember.Logger.debug;
-    Ember.Logger.debug = function() {
-      if (window.Rollbar) {
-        Rollbar.debug.apply(Rollbar, arguments);
-      }
-      debugLogger.apply(this, arguments);
-    };
+        if (window.Rollbar) {
+          Rollbar.error.call(Rollbar, err);
+        }
+        errorLogger.apply(this, arguments);
+      };
+      var warnLogger = Ember.Logger.warn;
+      Ember.Logger.warn = function() {
+        if (window.Rollbar) {
+          Rollbar.warning.apply(Rollbar, arguments);
+        }
+        warnLogger.apply(this, arguments);
+      };
+      var infoLogger = Ember.Logger.info;
+      Ember.Logger.info = function() {
+        if (window.Rollbar) {
+          Rollbar.info.apply(Rollbar, arguments);
+        }
+        infoLogger.apply(this, arguments);
+      };
+      var debugLogger = Ember.Logger.debug;
+      Ember.Logger.debug = function() {
+        if (window.Rollbar) {
+          Rollbar.debug.apply(Rollbar, arguments);
+        }
+        debugLogger.apply(this, arguments);
+      };
+    }
   }
 };
 
